1ad89e8cd6047d5d55a7b5ee8da82628396be862
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2017-09-13 15:27:39 +0100

    First commit

---
+++ config/element-api.php
@@ -0,0 +1,36 @@
+<?php
+
+use craft\elements\Entry;
+use craft\helpers\UrlHelper;
+
+return [
+    'endpoints' => [
+        'news.json' => [
+            'elementType' => Entry::class,
+            'criteria' => ['section' => 'funding'],
+            'transformer' => function(Entry $entry) {
+                return [
+                    'title' => $entry->title,
+                    'url' => $entry->url,
+                    'jsonUrl' => UrlHelper::url("news/{$entry->id}.json"),
+                    'summary' => $entry->body,
+                ];
+            },
+        ],
+        'news/<entryId:\d+>.json' => function($entryId) {
+            return [
+                'elementType' => Entry::class,
+                'criteria' => ['id' => $entryId],
+                'one' => true,
+                'transformer' => function(Entry $entry) {
+                    return [
+                        'title' => $entry->title,
+                        'url' => $entry->url,
+                        'summary' => $entry->summary,
+                        'body' => $entry->body,
+                    ];
+                },
+            ];
+        },
+    ]
+];
---
+++ config/general.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
+ */
+
+return [
+    // Global settings
+    '*' => [
+        // Default Week Start Day (0 = Sunday, 1 = Monday...)
+        'defaultWeekStartDay' => 0,
+
+        // Enable CSRF Protection (recommended, will be enabled by default in Craft 3)
+        'enableCsrfProtection' => true,
+
+        // Whether "index.php" should be visible in URLs
+        'omitScriptNameInUrls' => true,
+
+        // Control Panel trigger word
+        'cpTrigger' => 'admin',
+    ],
+
+    // Dev environment settings
+    'dev' => [
+        // Base site URL
+        'siteUrl' => null,
+
+        // Dev Mode (see https://craftcms.com/support/dev-mode)
+        'devMode' => true,
+    ],
+
+    // Staging environment settings
+    'staging' => [
+        // Base site URL
+        'siteUrl' => null,
+    ],
+
+    // Production environment settings
+    'production' => [
+        // Base site URL
+        'siteUrl' => null,
+    ],
+];
