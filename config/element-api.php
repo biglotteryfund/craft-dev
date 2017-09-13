3e260c3723d63fecf3445a2feff9109a72b23103
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2017-09-13 16:11:47 +0100

    Explore the element API fields

--- config/element-api.php
+++ config/element-api.php
@@ -7,16 +7,40 @@ return [
     'endpoints' => [
         'news.json' => [
             'elementType' => Entry::class,
-            'criteria' => ['section' => 'funding'],
+            'criteria' => [
+                'section' => 'funding',
+                'site' => 'default'
+            ],
             'transformer' => function(Entry $entry) {
                 return [
                     'title' => $entry->title,
                     'url' => $entry->url,
                     'jsonUrl' => UrlHelper::url("news/{$entry->id}.json"),
-                    'summary' => $entry->body,
+                    'body' => $entry->body,
+                    'heroText' => $entry->text,
+                    'heroImage' => $entry->image->first()['filename'],
+                    'heroLink' => $entry->herolink->first(),
                 ];
             },
         ],
+        'welsh/news.json' => [
+                'elementType' => Entry::class,
+                'criteria' => [
+                    'section' => 'funding',
+                    'site' => 'blfWelsh'
+                ],
+                'transformer' => function(Entry $entry) {
+                    return [
+                        'title' => $entry->title,
+                        'url' => $entry->url,
+                        'jsonUrl' => UrlHelper::url("news/{$entry->id}.json"),
+                        'body' => $entry->body,
+                        'heroText' => $entry->text,
+                        'heroImage' => $entry->image->first()['filename'],
+                        'heroLink' => $entry->herolink->first(),
+                    ];
+                },
+            ],
         'news/<entryId:\d+>.json' => function($entryId) {
             return [
                 'elementType' => Entry::class,
