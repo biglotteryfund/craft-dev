7c94a7f6320915dd0b29103d0192dcd8c04574d5
David Rapson <david.rapson@biglotteryfund.org.uk>
2017-10-11 15:40:55 +0100

    Fix deprecation warnings

--- config/general.php
+++ config/general.php
@@ -20,6 +20,8 @@ return [
 
         // Control Panel trigger word
         'cpTrigger' => 'admin',
+
+        'securityKey' => 'DugP0KMpm7-KvS9ID8vDmL3dPXs8S0uN'
     ],
 
     // Dev environment settings
