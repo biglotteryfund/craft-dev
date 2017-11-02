7792586ec2a893862491f27cea7b641d39aa408d
David Rapson <david.rapson@biglotteryfund.org.uk>
2017-11-02 15:26:08 +0000

    Add countries outside the UK to translations

--- translations/cy/site.php
+++ translations/cy/site.php
@@ -7,6 +7,7 @@ return array(
     "Scotland" => "Yr Alban",
     "Northern Ireland" => "Gogledd Iwerddon",
     "UK-wide" => "Ledled y DU",
+    "Countries outside the UK" => "Gwledydd y tu allan i'r DU",
 
     // Organisation types
     "Voluntary or community organisation" => "Mudiad gwirfoddol neu gymunedol",
