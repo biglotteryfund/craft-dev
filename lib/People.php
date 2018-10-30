fdf1318c1b4124583e231cae41daff5f54d5b711
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-10-30 12:36:07 +0000

    Rework our people endpoint

--- lib/People.php
+++ lib/People.php
@@ -18,30 +18,38 @@ class PeopleTransformer extends TransformerAbstract
         list('entry' => $entry, 'status' => $status) = EntryHelpers::getDraftOrVersionOfEntry($entry);
         $common = ContentHelpers::getCommonDetailFields($entry, $this->locale);
 
-        $documents = $entry->documents->one();
-
         return array_merge($common, [
             'people' => array_map(function ($person) {
+                $image = $person->personPhoto->one() ?? null;
                 return [
                     'name' => $person->personName,
                     'role' => $person->personRole ?? null,
-                    'image' => Images::extractImageUrl($person->personPhoto),
-                    'bio' => $person->personBio
+                    'image' => $image ? [
+                        // Is the source image large or small?
+                        // Used to determine what layout to use
+                        'type' => $image->width > 500 ? 'large' : 'small',
+                        'url' => Images::imgixUrl(
+                            $image->url,
+                            ['fit' => 'crop', 'crop' => 'entropy', 'max-w' => 1200]
+                        ),
+                    ] : null,
+                    'bio' => $person->personBio,
                 ];
             }, $entry->people->all() ?? []),
-            'documents' => $documents ? [
-                'title' => $documents->documentsTitle,
-                'files' => array_map(function ($document) {
-                    $file = $document->documentFile->one();
-                    return [
-                        'label' => $document->documentTitle,
-                        'caption' => $document->documentDescription ?? null,
-                        'href' => $file->url,
-                        'filetype' => $file->kind,
-                        'filesize' => StringHelpers::formatBytes($file->size, $precision = 0),
-                    ];
-                }, $documents->documents->all() ?? []),
-            ] : null
+            'documentGroups' => array_map(function ($group) {
+                return [
+                    'title' => $group->documentsTitle,
+                    'files' => array_map(function ($file) {
+                        return [
+                            'label' => $file->title,
+                            'href' => $file->url,
+                            'filetype' => $file->extension,
+                            'filesize' => StringHelpers::formatBytes($file->size, $precision = 0),
+                        ];
+                    }, $group->documentsFiles->all() ?? []),
+                    'extraContent' => $group->documentsExtra ?? null,
+                ];
+            }, $entry->documentGroups->all() ?? []),
         ]);
     }
 }
