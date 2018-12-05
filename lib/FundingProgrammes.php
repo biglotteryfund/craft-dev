19790408067c970eab59e6ac196f1ea48c2a5794
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-12-05 16:09:35 +0000

    Remove v1 of funding programmes endpoint

--- lib/FundingProgrammes.php
+++ lib/FundingProgrammes.php
@@ -7,64 +7,7 @@ use biglotteryfund\utils\Images;
 use craft\elements\Entry;
 use League\Fractal\TransformerAbstract;
 
-// @TODO: Replace this with FundingProgrammeTransformerNew when moving to new api endpoint
 class FundingProgrammeTransformer extends TransformerAbstract
-{
-    public function __construct($locale)
-    {
-        $this->locale = $locale;
-    }
-
-    public function transform(Entry $entry)
-    {
-        list('entry' => $entry, 'status' => $status) = EntryHelpers::getDraftOrVersionOfEntry($entry);
-
-        // Use custom thumbnail if one is set, otherwise default to hero image.
-        $heroImage = Images::extractImage($entry->heroImage);
-        $thumbnailSrc = Images::extractImage($entry->trailPhoto) ??
-            ($heroImage ? $heroImage->imageMedium->one() : null);
-
-        $data = [
-            'id' => $entry->id,
-            'availableLanguages' => EntryHelpers::getAvailableLanguages($entry->id, $this->locale),
-            'status' => $status,
-            'dateUpdated' => $entry->dateUpdated,
-            'title' => $entry->title,
-            'url' => $entry->url,
-            'path' => $entry->uri,
-            'hero' => Images::extractHeroImage($entry->heroImage),
-            'heroCredit' => $entry->heroImageCredit ?? null,
-            'intro' => $entry->programmeIntro,
-            'footer' => $entry->outroText ?? null,
-            'summary' => getFundingProgramMatrix($entry, $this->locale)
-        ];
-
-        $contentSections = [];
-        if ($entry->programmeRegions) {
-            foreach ($entry->programmeRegions->all() as $block) {
-                switch ($block->type->handle) {
-                    case 'programmeRegion':
-                        $region = [
-                            'title' => $block->programmeRegionTitle,
-                            'body' => $block->programmeRegionBody,
-                        ];
-                        array_push($contentSections, $region);
-                        break;
-                }
-            }
-        }
-
-        $data['contentSections'] = $contentSections;
-
-        if ($entry->relatedCaseStudies) {
-            $data['caseStudies'] = EntryHelpers::extractCaseStudySummaries($entry->relatedCaseStudies->all());
-        }
-
-        return $data;
-    }
-}
-
-class FundingProgrammeTransformerNew extends TransformerAbstract
 {
     public function __construct($locale)
     {
@@ -89,7 +32,7 @@ class FundingProgrammeTransformerNew extends TransformerAbstract
                 'h' => 100,
                 'crop' => 'faces',
             ]) : null,
-            'contentSections' => array_map(function($block) {
+            'contentSections' => array_map(function ($block) {
                 return [
                     'title' => $block->programmeRegionTitle,
                     'body' => $block->programmeRegionBody,
@@ -101,9 +44,9 @@ class FundingProgrammeTransformerNew extends TransformerAbstract
             ] : null,
             'fundingSize' => [
                 'minimum' => $entry->minimumFundingSize ? (int) $entry->minimumFundingSize : null,
-                'maximum' => $entry->maximumFundingSize ? (int) $entry->maximumFundingSize  : null,
+                'maximum' => $entry->maximumFundingSize ? (int) $entry->maximumFundingSize : null,
                 'totalAvailable' => $entry->totalFundingAvailable ?? null,
-                'description' => $entry->fundingSizeDescription ?? null
+                'description' => $entry->fundingSizeDescription ?? null,
             ],
             'applicationDeadline' => $entry->applicationDeadline ?? null,
             'organisationType' => $entry->organisationType ?? null,
