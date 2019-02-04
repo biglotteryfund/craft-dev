f923ece5bb228875d53f0b30ad163e5ba8ecfe97
Matt Andrews <matt.andrews@tnlcommunityfund.org.uk>
2019-02-04 12:50:27 +0000

    Use traditional analytics.js GA technique instead of new GTM version

--- web/resources/js/analytics.js
+++ web/resources/js/analytics.js
@@ -1,13 +1,10 @@
+window.ga = window.ga || function () {
+    (ga.q = ga.q || []).push(arguments)
+};
+ga.l = +new Date();
+ga('create', 'UA-637620-33', 'auto');
+ga('send', 'pageview');
 var ref = window.document.getElementsByTagName('script')[0];
 var script = window.document.createElement('script');
-script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-637620-33';
+script.src = 'https://www.google-analytics.com/analytics.js';
 ref.parentNode.insertBefore(script, ref);
-
-window.dataLayer = window.dataLayer || [];
-
-function gtag() {
-    dataLayer.push(arguments);
-}
-
-gtag('js', new Date());
-gtag('config', 'UA-637620-33');
