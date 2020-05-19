999472bcd4de86d2db01b43d79915894c764612a
Matt Andrews <matt.andrews@tnlcommunityfund.org.uk>
2020-05-19 15:34:18 +0100

    One final permission change

--- deploy/start.sh
+++ deploy/start.sh
@@ -14,4 +14,4 @@ su -s /bin/bash -c 'cd /var/www/craft/ && ./craft migrate/all && ./craft project
 # Because php-fpm installs some things as apache:apache
 chown -R nginx:nginx /var/www/craft/
 cd /var/www/craft
-chmod -R 777 composer.json composer.lock storage vendor web/cpresources
+chmod -R 777 composer.json composer.lock storage vendor web/cpresources config
