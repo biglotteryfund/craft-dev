f57da8d38f3cd64a209dd8c7799a1d1d78387632
Matt Andrews <matt.andrews@tnlcommunityfund.org.uk>
2020-05-19 15:23:49 +0100

    Move permissions change to the end of the script

--- deploy/start.sh
+++ deploy/start.sh
@@ -4,6 +4,9 @@ set -e
 # ApplicationStart script
 #################################################
 
+# Run any migrations to the CMS and sync the project config
+su -s /bin/bash -c 'cd /var/www/craft/ && ./craft migrate/all && ./craft project-config/sync' nginx
+
 #################################################
 # Permissions
 #################################################
@@ -12,6 +15,3 @@ set -e
 chown -R nginx:nginx /var/www/craft/
 cd /var/www/craft
 chmod -R 777 composer.json composer.lock storage vendor web/cpresources
-
-# Run any migrations to the CMS and sync the project config
-su -s /bin/bash -c 'cd /var/www/craft/ && ./craft migrate/all && ./craft project-config/sync' nginx
