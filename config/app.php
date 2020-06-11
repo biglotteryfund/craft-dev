9d2527f40f176b5fd160b50211c717b254cb98a8
Matt Andrews <matt.andrews@tnlcommunityfund.org.uk>
2020-06-11 14:56:03 +0100

    Store session and cache in the database to avoid sharded clash on load-balanced environments

--- config/app.php
+++ config/app.php
@@ -4,6 +4,22 @@ return [
     'modules' => [
         'control-panel' => biglotteryfund\Module::class,
     ],
+    'components' => [
+        'session' => function() {
+            // Get the default component config
+            $config = craft\helpers\App::sessionConfig();
+
+            // Override the class to use DB session class
+            $config['class'] = yii\web\DbSession::class;
+
+            // Set the session table name
+            $config['sessionTable'] = craft\db\Table::PHPSESSIONS;
+
+            // Instantiate and return it
+            return Craft::createObject($config);
+        },
+        'cache' => craft\cache\DbCache::class,
+    ],
     'bootstrap' => [
         'control-panel',
     ],
