e8d2c9ab02884b43a694db477abd2de38a95c146
MDurvesh <mohammed.durvesh@tnlcommunityfund.org.uk>
2021-01-25 12:23:07 +0000

    refactored bash script to investigate issue

--- deploy/start.sh
+++ deploy/start.sh
@@ -15,7 +15,8 @@ cd /var/www/craft
 chmod -R 777 composer.json composer.lock storage vendor web/cpresources config config/license.key
 
 # Run any migrations to the CMS and sync the project config
-su -s /bin/bash -c 'cd /var/www/craft/ && ./craft migrate/all && ./craft project-config/sync' nginx
+su -s /bin/bash -c './var/www/craft/craft migrate/all' nginx
+su -s /bin/bash -c './var/www/craft/craft project-config/sync' nginx
 
 # Set permissions again
 # @TODO work out why we need this twice
