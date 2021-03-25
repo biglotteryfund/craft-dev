85d3565f01442507030367728ce091f9eba3511e
Mark Smith <mark.smith@tnlcommunityfund.org.uk>
2021-03-25 09:29:55 +0000

    Fixed failing image test.

--- tests/ImagesTest.php
+++ tests/ImagesTest.php
@@ -9,7 +9,7 @@ final class ImagesTest extends TestCase
     public function testBuildsImgixUrl(): void
     {
         $this->assertRegExp(
-            '/image\.jpg\?auto=compress%2Cformat&crop=entropy&fit=crop&s=.*$/',
+            '/image\.jpg\?auto=compress%2Cformat&crop=faces%2Cedge&fit=crop&s=.*$/',
             Images::imgixUrl("https://media.example.com/path/to/image.jpg")
         );
 
